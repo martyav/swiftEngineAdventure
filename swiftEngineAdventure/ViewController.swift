8fd2efb44fab0eb9136e548c71b10c6f65840329
Marty Hernandez Avedon <martyavedon@gmail.com>
2017-10-20 06:11:58 -0400

    user can type in number and get requested index from swiftEngine API

--- swiftEngineAdventure/ViewController.swift
+++ swiftEngineAdventure/ViewController.swift
@@ -9,17 +9,67 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var userInput: UITextField!
+    
+    var manager: APIRequestManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.manager = APIRequestManager()
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func enterButtonWasTapped(_ sender: UIButton) {
+ //       let randomUserID = arc4random_uniform(UInt32.max)
+        
+//        if let userText = userInput.text {
+//            manager.storeData(endpoint: Enspoint.post, data: ["user\(randomUserID)": userText]) { (data: Data?) in
+//                if let validData = data {
+//                    do {
+//                        let json = try JSONSerialization.jsonObject(with: validData, options: [])
+//                        print(json)
+//                    }
+//                    catch {
+//                        print("error making post request")
+//                    }
+//                }
+//            }
+//
+//        }
+        guard let requestNumber = userInput?.text else { return }
+        
+        let getRequest = "\(Endpoint.get)=\(String(describing: requestNumber))"
+        
+        manager.getData(endPoint: getRequest) { (data: Data?) in
+            if let validData = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: validData, options: [])
+                    
+                    guard let results = json as? [String : Any],
+                        let response = results["response"] else {
+                        throw ParsedJSON.error
+                    }
+                    
+                    self.display(response)
+                }
+                catch {
+                    print("error making get request")
+                }
+            }
+        }
     }
-
-
+    
+    
+    func display(_ json: Any) {
+        let text = String(describing: json)
+        
+        DispatchQueue.main.async {
+            self.textView.text = text
+        }
+    }
 }
 
